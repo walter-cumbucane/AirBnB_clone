2a02771d0eb7701aa12109288b75d24257c1237c
walter-cumbucane <cumbucane.walter@gmail.com>
2023-11-09 01:44:05 +0200

    Let's get started

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+    Contains a base class that defines all common attributes/methods
+    for others classes
+"""
+import uuid
+import datetime
+
+
+class BaseModel(object):
+    """
+        Contains the base class of the project
+    """
+
+    def __init__(self):
+        """
+            Class's Constructor
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
+
+    def __str__(self):
+        """
+            Creates a printable object
+        """
+        printable = f"[{type(self).__name__}] ({self.id}) "
+        printable += f"{self.__dict__}"
+        return printable
+
+    def save(self):
+        """
+            Updates the updated_at with the current time
+        """
+        self.updated_at = datetime.datetime.now().isoformat()
+
+    def to_dict(self):
+        """
+            Returns a dictionary containing all keys/values of the
+            __dict__ of the instance
+        """
+        dictionary = {}
+        dictionary["id"] = self.id
+        dictionary["created_at"] = self.created_at
+        dictionary["updated_at"] = self.updated_at
+        dictionary["__class__"] = type(self).__name__
+        return dictionary
