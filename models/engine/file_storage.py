dfca062822841e98044204e380e0607383302f39
walter-cumbucane <cumbucane.walter@gmail.com>
2023-11-10 00:17:11 +0200

    Task 5 in the way...

---
+++ models/engine/file_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Contains a class that serializes instances to a JSON file 
+    and deserializes JSON files to instances
+"""
+from models.base_model import BaseModel
+
+
+class FileStorage(object):
+    """
+        Class Definition
+    """
+
+    __file
+
+    def __init__(self):
+        super().__init__()
+    
